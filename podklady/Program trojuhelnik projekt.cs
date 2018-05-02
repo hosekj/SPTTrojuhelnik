8db18b215a8622a7e01b2f3c906e0819db3af4ee
m1petras <38525187+m1petras@users.noreply.github.com>
2018-05-02 12:36:44 +0200

    Add files via upload

---
+++ podklady/Program trojuhelnik projekt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bod A
+            int x1 = 2; 
+            int y1 = 3;
+            //bod B
+            int x2 = 2;
+            int y2 = 3;
+            //bod C 
+            int x3 = 1;
+            int y3 = 1;
+            // dva body v sobe
+            if ((((x1 == x2) && (y1 == y2))) ||
+               (((x2 == x3) && (y2 == y3))) ||
+               (((x1 == x3) && (y1 == y3))))
+            {
+                Console.WriteLine("dva body v jednom bode");
+            }
+            //body na jedne primce 
+
+            //obsah 
+
+            //obvod
+            Console.ReadKey();
+            
+        }
+    }
+}
